12317c9e8fa130a9bc23bb44658569ae2cb532cd
Amer <thelordamer2000@gmail.com>
2025-03-13 06:47:52 +0300

    V1.5 add AOP Aspect-Oriented Programming

--- src/main/java/com/project/library_management_system/LibraryManagementSystem.java
+++ src/main/java/com/project/library_management_system/LibraryManagementSystem.java
@@ -4,9 +4,10 @@ package com.project.library_management_system;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @SpringBootApplication
-//@ComponentScan
+@EnableAspectJAutoProxy
 public class LibraryManagementSystem {
 
 	public static void main(String[] args) {
--- src/main/java/com/project/library_management_system/config/SecurityConfig.java
+++ src/main/java/com/project/library_management_system/config/SecurityConfig.java
@@ -2,7 +2,6 @@ package com.project.library_management_system.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
